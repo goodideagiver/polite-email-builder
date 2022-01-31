65485ac7609f643bc8884ec8c23b755d63317022
prplblck <kariurek@gmail.com>
2022-01-31 19:39:49 +0100

    CR fix

--- src/script/randomColorGenerator.js
+++ src/script/randomColorGenerator.js
@@ -1,16 +1,18 @@
 const colorGeneratorRGBA = (opacity = 0.4) => {
-	const min = Math.ceil(0);
-	const max = Math.floor(255);
-	const generatedColors = [];
+	const MIN_VALUE = 0,
+		MAX_VALUE = 255,
+		generatedColors = [];
 	for (let i = 0; i < 3; i++) {
-		generatedColors.push(Math.floor(Math.random() * (max - min) + min));
+		generatedColors.push(
+			Math.floor(Math.random() * (MAX_VALUE - MIN_VALUE) + MIN_VALUE)
+		);
 	}
 	return `rgba(${generatedColors[0]},${generatedColors[1]},${generatedColors[2]},${opacity})`;
 };
 
 const backgroundColorGradient = () => {
-	const websiteBody = document.querySelector('body');
-	const generatedColors = [];
+	const websiteBody = document.querySelector('body'),
+		generatedColors = [];
 	for (let i = 0; i < 2; i++) {
 		generatedColors.push(colorGeneratorRGBA(0.1));
 	}
