dc1ff264e45df580cef281ba42607b019d029b68
prplblck <kariurek@gmail.com>
2022-01-31 17:54:38 +0100

    test

--- src/script/scrollShadow.js
+++ src/script/scrollShadow.js
@@ -1,1 +1,18 @@
 const shadowScrollContainers = document.querySelectorAll('.sscroll');
+
+const applyShadow = (e) => {
+	const container = e.target;
+	const maxScroll = container.clientHeight;
+	console.log('maxScroll :', maxScroll);
+	const currentScrollPosition = container.scrollTop;
+	console.log('currentScrollPosition :', currentScrollPosition);
+	if (currentScrollPosition < maxScroll - 10) {
+		container.classList.add('bottom-box-shadow');
+	} else {
+		container.classList.remove('bottom-box-shadow');
+	}
+};
+
+shadowScrollContainers.forEach((container) => {
+	container.addEventListener('scroll', applyShadow);
+});
