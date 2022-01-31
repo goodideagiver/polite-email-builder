034877c95cfab9b0a7f815beddcad385413dfd92
prplblck <kariurek@gmail.com>
2022-02-01 00:55:47 +0100

    tooltip.js start

---
+++ src/script/functions/tooltip.js
@@ -0,0 +1,9 @@
+const tooltipNode = (nodeText) => {
+	const tooltipElement = document.createElement('div');
+	tooltipElement.classList.add('tooltip');
+	tooltipElement.innerText = nodeText;
+};
+
+const showTooltip = (tooltipAnchorElement) => {
+    
+};
